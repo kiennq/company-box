54e5a0086dd4f267062231e0782bc716c1e8f4d9
Kien Nguyen <kien.n.quang@gmail.com>
2020-08-05 14:20:41 +0900

    company-mode: better integration with company-selection

--- company-box-icons.el
+++ company-box-icons.el
@@ -50,7 +50,7 @@
               :height ,(round (* (default-font-height) 0.85))
               :ascent center))))
 
-(defvar company-box-icons-icons-in-terminal
+(defconst company-box-icons-icons-in-terminal
   '((Unknown fa_question_circle)
     (Text . fa_text_height) ;; Text
     (Method . (fa_tags :face font-lock-function-name-face)) ;; Method
@@ -80,7 +80,7 @@
     (Template . fa_bookmark)) ;; TypeParameter
   )
 
-(defvar company-box-icons-eclipse
+(defconst company-box-icons-eclipse
   (eval-when-compile
     `((Unknown . ,(company-box-icons-image "Namespace.png"))
       (Text . ,(company-box-icons-image "eclipse/text.png"))
@@ -110,7 +110,7 @@
       (TypeParameter . ,(company-box-icons-image "eclipse/typeparameter.png"))
       (Template . ,(company-box-icons-image "eclipse/template.png")))))
 
-(defvar company-box-icons-netbeans
+(defconst company-box-icons-netbeans
   (eval-when-compile
     `((Unknown . ,(company-box-icons-image "Namespace.png"))
       (Text . ,(company-box-icons-image "netbeans/text.png"))
@@ -140,7 +140,7 @@
       (TypeParameter . ,(company-box-icons-image "netbeans/typeparameter.png"))
       (Template . ,(company-box-icons-image "netbeans/template.png")))))
 
-(defvar company-box-icons-images
+(defconst company-box-icons-images
   (eval-when-compile
     `((Unknown . ,(company-box-icons-image "Namespace.png"))
       (Text . ,(company-box-icons-image "String.png"))
@@ -286,9 +286,11 @@ See `company-box-icons-images' or `company-box-icons-all-the-icons' for the ICON
 [1] https://github.com/Microsoft/language-server-protocol/blob/gh-pages/\
 specification.md#completion-request-leftwards_arrow_with_hook.")
 
+(declare-function lsp-get "ext:lsp-protocol")
+
 (defun company-box-icons--lsp (candidate)
   (-when-let* ((lsp-item (get-text-property 0 'lsp-completion-item candidate))
-               (kind-num (gethash "kind" lsp-item)))
+               (kind-num (lsp-get lsp-item :kind)))
     (alist-get kind-num company-box-icons--lsp-alist)))
 
 (defconst company-box-icons--php-alist
